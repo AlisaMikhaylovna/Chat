03931e6d633686b41ac1ea87dfe05313246075fc
zqx <2322023152@qq.com>
2024-09-04 13:56:20 +0800

    forth

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,6 +1,7 @@
 import { initialProfile } from "@/lib/initial-profile";
 import { db } from "@/lib/db"
 import { redirect } from "next/navigation";
+import { InitialModal } from "@/components/modals/initial-modal";
 
 const SetupPage = async () => {
     const profile = await initialProfile();
@@ -13,10 +14,10 @@ const SetupPage = async () => {
             }
         }
     });
-    if (server) 
+    if (server)
         return redirect(`/servers/${server.id}`);
     return (
-        <div>create a server</div>
+        <InitialModal />
     );
 }
 
